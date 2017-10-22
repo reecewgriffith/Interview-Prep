6480da8b6a1e4db9dc8ac3538d497c734ef2333e
Reece W Griffith <rgriff43@jhu.edu>
2017-10-22 15:20:49 -0400

    first occur and last occur

---
+++ Search/Binary/lastOccur.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+/**
+ * Basic Binary Search (Iterative) 
+ */
+public class lastOccur {
+	
+	public static int binSearchEle(int arr[], int ele) {
+
+		int low = 0;
+		int high = arr.length - 1;
+		int index = -1;
+
+		while (high >= low) {
+			int mid = (high + low) / 2;
+
+			if (arr[mid] == ele) {
+				index = mid;
+				low = mid + 1;
+			}
+			else if (arr[mid] > ele) {
+				high = mid - 1;
+			}
+			else {
+				low = mid + 1;
+			}
+		}
+		return index;
+	}
+
+	public static void main(String[] args) {
+
+		int arr[] = { 102, 366, 7, 7, 7, 9, 2, 1, 15, 22, 36, 58};
+
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println("");
+
+		System.out.println("INDEX: " + binSearchEle(arr, 7));
+	}
+
+}
