82ab5a1ebd732c8c5ab84ec5e27edf4f11d7ea73
Reece W Griffith <rgriff43@jhu.edu>
2017-12-25 14:07:11 -0800

    selection sort

--- C++/Algorithms/Sort/Bubble/bubble.cpp
+++ C++/Algorithms/Sort/Bubble/bubble.cpp
@@ -13,9 +13,9 @@ void bubbleSort(int arr[], int siz) {
     int a;
     int b;
 
-    for (int i = 0; i < siz; i++) {
+    for (int i = 0; i < siz - 1; i++) {
         for (int j = 0; j < siz - i - 1; j++) {
-            if (arr[i] > arr[j]) {
+            if (arr[j] > arr[j+1]) {
                 swap(&arr[j], &arr[j+1]);
             }
         }
---
+++ C++/Algorithms/Sort/Selection/select.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void selectionSort(int arr[], int size) {
+
+    int min;
+    for (int i = 0; i < size; i++) {
+
+        min = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+
+        swap(&arr[min], &arr[i]);
+    }
+
+}
+
+int main() {
+
+    int arr[] = {10, 7, 9, 102, 155, 81, 67, 4, 0, 31};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    selectionSort(arr, size);
+
+    for (int z = 0; z < size; z++) {
+        cout << arr[z] << " ";
+    }
+
+    return 0;
+}
