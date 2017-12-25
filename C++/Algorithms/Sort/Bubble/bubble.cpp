53f550c3ce717408b76d2b00e6abb123b37601e7
Reece W Griffith <rgriff43@jhu.edu>
2017-12-24 17:36:31 -0800

    bubble

---
+++ C++/Algorithms/Sort/Bubble/bubble.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+
+}
+
+void bubbleSort(int arr[], int siz) {
+    int a;
+    int b;
+
+    for (int i = 0; i < siz; i++) {
+        for (int j = 0; j < siz - i - 1; j++) {
+            if (arr[i] > arr[j]) {
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
+
+int main() {
+
+    int arr[] = {1, 65, 221, 0, 25, 99, 5, 20, 12, 9, 8};
+    int siz = sizeof(arr)/sizeof(arr[0]);
+
+    bubbleSort(arr, siz);
+
+    for (int z = 0; z < siz; z++) {
+        cout << arr[z] << " ";
+    }
+
+    return 0;
+}
