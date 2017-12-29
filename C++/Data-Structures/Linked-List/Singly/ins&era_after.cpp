27b6b763fba8a59cb7bff6fdf606158b32d6bb9a
Reece W Griffith <rgriff43@jhu.edu>
2017-12-29 10:04:23 -0800

    Re-add forward list and list STL

---
+++ C++/Data-Structures/Linked-List/Singly/ins&era_after.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <forward_list>
+
+using namespace std;
+using std::endl;
+
+int main() {
+
+    forward_list<int> flist = {10, 25, 77, 102};
+    forward_list<int>::iterator it;
+
+    //before_begin is location before first elem
+    //begin() is location of first element
+    it = flist.insert_after(flist.before_begin(), 99);
+    
+    //cant do because removes iterator
+    //flist.pop_front();
+
+    //FORM iterator = list_name.insert_after(iterator, # of elems, value for elems);
+    it = flist.insert_after(it, 4, 101);
+
+    cout << endl;
+    cout << "FLIST: ";
+    for (int&a : flist) {
+        cout << a << " ";
+    }
+    cout << endl << endl;
+
+    return 0;
+}
